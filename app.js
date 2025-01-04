2dcae912705be11e4ccf9081c86dc1cb164966c9
mahdibkt44444444 <you@example.com>
2025-01-04 14:19:33 +0100

    connection DB

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require("express");
+const app = express();
+const port = 3001;
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
-  res.sendFile("./views/home.html", { root: __dirname })
-})
+app.get("/", (req, res) => {
+  res.sendFile("./views/home.html", { root: __dirname });
+});
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+
+
+mongoose
+  .connect("mongodb+srv://mahdibkt:iHnuyDXJl6AOMyAK@cluster0.myqej.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => {
+    app.listen(port, () => {
+        console.log(`http://localhost:${port}/`);
+      });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
