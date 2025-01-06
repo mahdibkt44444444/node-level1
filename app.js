cf397826c9d2edcd121dceeb4ffcb28d368631f2
mahdibkt44444444 <you@example.com>
2025-01-06 18:40:59 +0100

    find

--- app.js
+++ app.js
@@ -3,42 +3,51 @@ const app = express();
 const port = 3001;
 const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
-const Mydata=require("./models/mydataSchema")
-
+const Mydata = require("./models/mydataSchema");
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/home.html", { root: __dirname });
+  // result ==> array of objects
+
+  Mydata.find()
+    .then((result) => {
+      res.render("home", { mytitle: "Home page", arr: result });
+      
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
 });
 
 app.get("/index.html", (req, res) => {
-    res.send("<h1>hellobbbb</h1>");
-  });
-
-
+  res.send("<h1>SUCCESS</h1>");
+});
 
 mongoose
-  .connect("mongodb+srv://mahdibkt:iHnuyDXJl6AOMyAK@cluster0.myqej.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0")
+  .connect(
+    "mongodb+srv://mahdibkt:iHnuyDXJl6AOMyAK@cluster0.myqej.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(() => {
     app.listen(port, () => {
-        console.log(`http://localhost:${port}/`);
-      });
+      console.log(`http://localhost:${port}/`);
+    });
   })
   .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
   });
 
+app.post("/", (req, res) => {
+  console.log(req.body);
 
+  const mydata = new Mydata(req.body);
 
-  app.post("/", (req, res) => {
-    console.log(req.body);
-
-    const mydata = new Mydata(req.body);
-
-    mydata.save().then(() => {
-        res.redirect("/index.html");
-    }).catch((err) => {
+  mydata
+    .save()
+    .then(() => {
+      res.redirect("/index.html");
+    })
+    .catch((err) => {
       console.log(err);
     });
-
-    
-  });
+});
