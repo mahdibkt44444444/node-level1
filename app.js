91fee1a5baaba6ad2ce8185c4a3ec85d6bef11af
mahdibkt44444444 <you@example.com>
2025-01-08 10:32:54 +0100

    save data

--- app.js
+++ app.js
@@ -3,48 +3,59 @@ const app = express();
 const port = 3001;
 const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
-const Mydata = require("./models/mydataSchema");
+const User = require("./models/customerSchema");
 app.set("view engine", "ejs");
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // Auto refresh
- 
+
 const path = require("path");
 const livereload = require("livereload");
 const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(path.join(__dirname, 'public'));
- 
- 
+liveReloadServer.watch(path.join(__dirname, "public"));
+
 const connectLivereload = require("connect-livereload");
 app.use(connectLivereload());
- 
+
 liveReloadServer.server.once("connection", () => {
   setTimeout(() => {
     liveReloadServer.refresh("/");
   }, 100);
 });
 
-
+// GET Request
 
 app.get("/", (req, res) => {
   // result ==> array of objects
-  res.render("index", {  });
-  
-
+  res.render("index", {});
 });
 
 app.get("/user/add.html", (req, res) => {
-  res.render("user/add")
+  res.render("user/add");
 });
 
 app.get("/user/view.html", (req, res) => {
-  res.render("user/view")
+  res.render("user/view");
 });
 
 app.get("/user/edit.html", (req, res) => {
-  res.render("user/edit")
+  res.render("user/edit");
 });
 
+// POST Request
+
+app.post("/user/add.html", (req, res) => {
+  
+  const user = new User(req.body);
+  user
+    .save()
+    .then(() => {
+      res.redirect("/user/add.html");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 // connection
 mongoose
@@ -57,7 +68,5 @@ mongoose
     });
   })
   .catch((err) => {
-    console.log( err);
+    console.log(err);
   });
-
-
